427c650bdd3d1a94ac0989d41bbf2c4f4dc4a248
Ivaylo Evtimov <evtimov9@gmail.com>
2018-07-23 15:21:14 +0300

    Add User Model + Login and Register Logic

--- CarSalon_Project/CarSalon_App/urls.py
+++ CarSalon_Project/CarSalon_App/urls.py
@@ -6,4 +6,7 @@ urlpatterns = [
     # App Entry Urls
     path('',  views.application_index,     name='appIndex'),
     path('index', views.application_index,  name ="index"),
+    path('login/', auth_views.login, {'template_name': 'CarSalon_App/auth/login.html'}, name='login'),
+    path('register/', views.register, name='login'),
+    path('viewUsers',views.view_all_users,name='usrs')
 ]
--- CarSalon_Project/CarSalon_App/views.py
+++ CarSalon_Project/CarSalon_App/views.py
@@ -1,4 +1,30 @@
 from django.shortcuts import render
+from .templates.CarSalon_App.auth.registerForm import RegisterForm 
+from django.contrib.auth.models import User
+from django.contrib.auth import get_user_model
+
 
 def application_index(request):
-    return render(request,'../templates/index.html')
+    return render(request,'../templates/CarSalon_App/index.html')
+
+def register(request):
+    if request.method == 'POST':
+        form = RegisterForm(request.POST)
+        if form.is_valid():
+            form.save()
+            username = form.cleaned_data.get('username')
+            raw_password = form.cleaned_data.get('password1')
+            #user = authenticate(username=username, password=raw_password)
+            #login(request, user)
+
+    else:
+        form = RegisterForm()
+    return render(request, 'CarSalon_App/auth/register.html', {'form': form})       
+
+def view_all_users(request):
+   
+    users = User.objects.values('first_name','last_name','username','email','is_superuser')
+
+    context = {'users': users}
+
+    return render (request,'CarSalon_App/user/index.html',context)
